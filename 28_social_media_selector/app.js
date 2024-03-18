2905c9365987421ee506ff5207e6e5953721dbfc
Vishal Chauhan <151999539+Chauhanvishal01@users.noreply.github.com>
2024-03-18 17:10:05 +0530

    Social_Media_Selector
    Check out the image for a visual representation of the project's functionality!

---
+++ 28_social_media_selector/app.js
@@ -0,0 +1,20 @@
+const selectField = document.getElementById("selectField");
+const selectText = document.getElementById("selectText");
+const list = document.getElementById("list");
+const arrowIcon = document.getElementById("arrowIcon");
+const options = document.querySelectorAll(".options");
+
+selectField.addEventListener('click',()=>{
+    list.classList.toggle('hide')
+    arrowIcon.classList.toggle('rotate')
+})
+
+
+for(option of options){
+    option.onclick=function(){
+        selectText.innerHTML=this.textContent;
+        list.classList.toggle('hide')
+        arrowIcon.classList.toggle('rotate')
+
+    }
+}
